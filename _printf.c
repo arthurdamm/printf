2707885156b81e0ea15826f76f9ef09a608833a8
Arthur Damm <indigoarthur@gmail.com>
2019-03-17 21:53:01 +0000

    make sure 1 char

--- _printf.c
+++ _printf.c
@@ -14,7 +14,7 @@ int _printf(const char *format, ...)
 
 	va_start(ap, format);
 
-	if (!format || *format == '%')
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	for (p = (char *)format; *p; p++)
 	{
