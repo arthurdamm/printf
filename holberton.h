82f5a69186fb385f99fc77322e21f4a0d6a0eae6
Farrukh <679@holbertonschool.com>
2019-03-17 06:39:20 +0000

    Added prototype for octal, unsigned, hex and HEX functions

--- holberton.h
+++ holberton.h
@@ -27,12 +27,16 @@ int _puts(char *str);
 int print_char(va_list ap);
 int print_int(va_list ap);
 int print_string(va_list ap);
-int print_percent(va_list ap);
+int print_percent();
 
 int print_number(int n);
 
 int get_print_func(char *s, va_list ap);
 int _printf(const char *format, ...);
 char *convert(unsigned int num, int base, short l);
+int print_octal(va_list ap);
+int print_unsigned(va_list ap);
+int print_hex(va_list ap);
+int print_HEX(va_list ap);
 
 #endif
