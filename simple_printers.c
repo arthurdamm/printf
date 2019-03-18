9d17d63bdd8e53fdf8e8f2339cbe7ab8a6325417
Arthur Damm <indigoarthur@gmail.com>
2019-03-18 23:57:42 +0000

    put some rot13 back

--- simple_printers.c
+++ simple_printers.c
@@ -50,6 +50,23 @@ int print_rev(va_list ap, params_t *params)
 int print_rot13(va_list ap, params_t *params)
 {
 	(void)params;
-	(void)ap;
-	return (0);
+	int i, index;
+	int count = 0;
+	char arr[] = "NOPQRSTUVWXYZABCDEFGHIJKLM      nopqrstuvwxyzabcdefghijklm";
+	char *a = str_concat("", va_arg(ap, char *));
+
+	i = 0;
+	index = 0;
+	while (a[i])
+	{
+		if ((a[i] >= 'A' && a[i] <= 'Z') || (a[i] >= 'a' && a[i] <= 'z'))
+		{
+			index = a[i] - 65;
+			a[i] = arr[index];
+		}
+		i++;
+	}
+	count += _puts(a);
+	free(a);
+	return (count);
 }
