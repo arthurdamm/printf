060ec3a6e249e324e1b815b1c48dc75e4896429e
Arthur Damm <indigoarthur@gmail.com>
2019-03-18 20:07:46 +0000

    update tester

--- test/0-main.c
+++ test/0-main.c
@@ -7,8 +7,6 @@
  */
 int main(void)
 {
-    printf("%d\n", _printf("Hello %c%d ZY%fZ %s%e WEW", 'H', 321, 3.14, "lberton"));
-    _printf("%%%s%d%e\n", NULL, INT_MIN);
-    printf("<%d>\n", _printf("%%%s[%+ d]\n", NULL, -2));
+    printf("%lhd\n", 35);
     return (0);
 }
