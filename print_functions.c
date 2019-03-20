466824551eb0612d4e448559d6eee567f57a38a6
Arthur Damm <indigoarthur@gmail.com>
2019-03-20 03:15:48 +0000

    fix buffer bug?

--- print_functions.c
+++ print_functions.c
@@ -50,10 +50,11 @@ int print_string(va_list ap, params_t *params)
 {
 	char *str = va_arg(ap, char *);
 
+	(void)params;
 	switch ((int)(!str))
 		case 1:
 			str = NULL_STRING;
-	str = _str_copy_from_back(params->buf, str);
+	/* str = _str_copy_from_back(params->buf, str); */
 	return (_puts(str));
 }
 
