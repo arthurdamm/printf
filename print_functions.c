838d9a8902b6781f88a3d92d3b1a18983f5947b3
Arthur Damm <indigoarthur@gmail.com>
2019-03-21 05:04:19 +0000

    left shift %c

--- print_functions.c
+++ print_functions.c
@@ -10,11 +10,14 @@
 int print_char(va_list ap, params_t *params)
 {
 	char pad_char = ' ';
-	unsigned int pad = 1, sum = 0;
+	unsigned int pad = 1, sum = 0, ch = va_arg(ap, int);
 
+	if (params->minus_flag)
+		sum += _putchar(ch);
 	while (pad++ < params->width)
 		sum += _putchar(pad_char);
-	sum += _putchar(va_arg(ap, int));
+	if (!params->minus_flag)
+		sum += _putchar(ch);
 	return (sum);
 }
 
